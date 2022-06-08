99bdd1e5021aa5b2dfd93c8657870d5a8ee7bae4
Derek Cooper <derekcooper20@gmail.com>
2022-06-08 16:27:00 -0600

    Comp changes

--- comparisons.js
+++ comparisons.js
@@ -16,6 +16,7 @@ var numberStudents = 20
 var stringTeachers = "4"
 var numberDogs = 0
 
+
 // EXAMPLE: log the result of the comparison: is numberTeachers greater than numberStudents?
 console.log("Is numberTeachers greater than numberStudents?", numberTeachers > numberStudents);
 // this should log: "Is numberTeachers greater than numberStudents?" false
