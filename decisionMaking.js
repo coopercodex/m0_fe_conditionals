312a1e3a777585fd915412cf312a76ecc8f23895
Derek Cooper <derekcooper20@gmail.com>
2022-06-08 18:31:33 -0600

    Added paths

--- decisionMaking.js
+++ decisionMaking.js
@@ -35,9 +35,16 @@ if (bearChoice === 1) {
 
 // Questions
 // 1. In English, using technical vocabulary, describe what is happening between lines 12 and 16.
+  // On line line 12 if the doorchoice is 1 that means the bearclothing is a hat which is a string, if the doorchoice is not 1 then bearclothing is a scarf
 // 2. What variable has a new value assigned to it after the first if statement executes?
+  // after the first it looks like bearClothing has changed to scarf if the doorChoicec was not 1
 // 3. If you changed the variable doorChoice to equal 3, what would the bearClothing value be?
+  // the bearClothing would be a scarf because it does not equal 1
 // 4. In English, using technical vocabulary, describe what is happening between lines lines 25 and 33.
+  // if the bearChoice is equal to 1 the log would say the string below it adding the bearClothing that was previously decided the code runs until true statement is found
 // 5. If you changed the variable bearChoice to equal 3, what will be the final outcome be?
+  // the code would run inside the second else if statement because it found the true statement
 // 6. If you changed the variable doorChoice to equal 1, and the variable bearChoice to equal 2, what will be the final outcome be?
+  // Everything says the same as the original code BUT the message will instead say "you tell the bear the hat is too small etc" because this is true
 // 7. What is your favorite ending?
+  // i would change the bearChoice to 5 and run the last string...pretty good bodyguard i suppose
